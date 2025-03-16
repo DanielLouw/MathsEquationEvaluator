ed8905bb6afd04075e741d88b035c50460d6570f
DanielLouw <daniellouw@gmail.com>
2025-03-16 21:19:28 +0200

    Add Unit Testing to solution and fix bug in EquationInformation.

--- Program.cs
+++ Program.cs
@@ -5,12 +5,7 @@ namespace MathsEquationEvaluator
     public class Program
     {
         static void Main(string[] args)
-        {
-            // Test the EvaluateEquation method with 5, 3(+), 3. Should output 8.
-            Console.WriteLine($"{EquationEvaluator.EvaluateEquation("5,3,3")}");
-
-            //TODO: Add code to create a dummy instance of the EquationProvider class and test the DisplayEquationInformation method.
-            
+        {            
         }
     }
 }
